6ffef7ac86895e6e46cf2dbb6590b222d9233819
zayanahmed925 <rjraju925@gmail.com>
2022-05-22 17:36:00 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Page/Home/Home';
 import Navbar from './Page/Shared/Navbar/Navbar';
 function App() {
   return (
     <div >
       <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Page/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Banner from './Banner';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>HOme</h2>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
