cdfa6241fe0c9ab7e3a7b217a5c891541ffb3e6e
zayanahmed925 <rjraju925@gmail.com>
2022-05-26 11:09:41 +0600

    review add and show ui

--- src/Page/Dashboard/AddReview.js
+++ src/Page/Dashboard/AddReview.js
@@ -1,9 +1,42 @@
 import React from 'react';
-
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 const AddReview = () => {
+    const [user, loading, error] = useAuthState(auth);
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const review = {
+            user: user,
+            comments: event.target.comments.value,
+            reviewStar: event.target.review.value,
+        }
+        fetch('http://localhost:5000/review', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(review)
+        })
+            .then(res => res.json())
+            .then(data => {
+
+                if (data.success) {
+
+                }
+            })
+    }
     return (
-        <div>
-            <h2>Review</h2>
+        <div className='flex items-center md:p-30'>
+            <div>
+                <h2 className='text-center my-10'>Please Review our service</h2>
+                <form onSubmit={handleSubmit} className='grid grid-cols-1 gap-5 justify-items-center mt-2'>
+                    <textarea class="textarea textarea-bordered w-80" name='comments' placeholder="Comments"></textarea>
+                    <input type="number" name="review" placeholder="Review out of 5" className="input input-bordered w-full max-w-xs" />
+
+                    <input type="submit" value='Review' className="btn btn-primary w-full max-w-xs" />
+                </form>
+            </div>
         </div>
     );
 };
--- src/Page/Home/Reviews.js
+++ src/Page/Home/Reviews.js
@@ -1,8 +1,43 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import Rating from 'react-rating';
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading/Loading';
 
 const Reviews = () => {
+    const { data: reviews, isLoading, refetch } = useQuery('reviews', () => fetch('http://localhost:5000/review', {
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()))
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
-        <div>
+        <div >
+            <p>{reviews.length}</p>
+            <div className='grid lg:grid-cols-2 gap-5  '>
+                {
+                    reviews.map(review => <div className=' card bg-base-100 shadow-xl'>
+                        <div className='card-body '>
+                            <h2 class="card-title">{review.comments}</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <Rating
+                                initialRating={review.reviewStar}
+                                emptySymbol={<FontAwesomeIcon icon={faStar} />}
+                                fullSymbol={<FontAwesomeIcon style={{ color: 'goldenrod' }} icon={faStar} />}
+                                readonly
+                            >
+
+                            </Rating>
+                        </div>
+                    </div>)
+                }
+            </div>
+
+
 
         </div>
     );
